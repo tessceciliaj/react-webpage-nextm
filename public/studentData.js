3be570116260c3ecd9edafdd514967806d8bf8a1
Ida Mäkelä <idamakela98@gmail.com>
2023-04-19 14:46:53 +0200

    made database file and added some info

---
+++ public/studentData.js
@@ -0,0 +1,50 @@
+// make id for every student with nanoid
+
+const studentData = [
+    {
+        id: `${}`,
+        name: "",
+        profession: "",
+        likedin: ,
+        linkedinUrl: "",
+        portfolio: ,
+        portfolioUrl: "",
+        email: "",
+        desc: ""
+    },
+    {
+        id: `${}`,
+        name: "Ivar Kronqvist",
+        profession: ["Immersive experiance creator", "Nurse"],
+        likedin: true,
+        linkedinUrl: "https://www.linkedin.com/in/ivar-kronqvist-86337324b/",
+        portfolio: true,
+        portfolioUrl: "https://ivarkronqvist.myportfolio.com/",
+        email: "Ivarkronqvist@gmail.com",
+        desc: "With a unique perspective on creative projects, I work in an Agile and open-minded way with creative people, drawing from my nursing experience to give them the conditions they need to make great stuff. As an XR generalist, I possess a wide range of skills and tools, and am adept at working with people and groups to deliver successful projects. My goal is to become an efficient and skilled project manager, leading projects from conception to implementation."
+    },
+    {
+        id: `${}`,
+        name: "Ardavan Masihabadi",
+        profession: ["Music Composer", "Mix Engineer", "Sound Designer"],
+        likedin: true,
+        linkedinUrl: "https://www.linkedin.com/in/ardavan-masihabadi-482a311b1",
+        portfolio: true,
+        portfolioUrl: "https://drive.google.com/drive/folders/1Spmooux1qmnOAnhi02w2Mxn9fQeH-1Ea",
+        email: "masihardavan99@gmail.com",
+        desc: "I was born 1999 with an inner passion of music. Studying Bachelor and Master of Multimedia Arts with a focus on audio and having the opportunity to work as Audio Producer in different media mediums such as advertisement, video games and podcasts gave me a clear vision of the overall procedures happening in media and entertainment industry. On top of all, I've had the honor to learn from some of the bests in the industry and teach those skills in my personal workshops, university-supported workshops and as teaching assistant in my university. Lastly, some may count this as my weakness, but when I start doing something, only the best quality satisfies me, even if the odds are not in my favor."
+    },
+        {
+        id: `${}`,
+        name: "Sheila Géa",
+        profession: ["Immersive Experience Creator (VR - AR )", "Architect "],
+        likedin: true,
+        linkedinUrl: "https://www.linkedin.com/in/sheilagea/",
+        portfolio: true,
+        portfolioUrl: "https://www.sheiladgea.com/",
+        email: "sheilagea@gmail.com",
+        desc: "Hello! It is nice to meet you, I'm Sheila! I was born and raised in Brazil, but I'm currently living in Stockholm, Sweden.I have a degree in Architecture and Urbanism and a Postgraduate Degree in Project Management. Presently I am in the process of making my career transition to the Immersive Experience Creator. I have been dedicating my time to studying this area and currently doing my high vocational program. It would be a privilege to have the opportunity to share with you and your company my different views of the world. If you have any questions or require any further information, feel free to contact me. I'll be delighted to discuss my qualifications and interests."
+    },
+]
+
+export default studentData;
