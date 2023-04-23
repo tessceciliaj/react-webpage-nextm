6c9ff734317a47cb733e78faedcaa41da25204fb
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-04-23 16:16:03 +0200

    add student data in StudentCard

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,9 +1,21 @@
-export default function StudentCard(data) {
+import styles from '../styles/students.module.css'
+import studentPic from '../assets/student.png'
+
+export default function StudentCard({img, name, profession, email, portfolioUrl}) {
     return (
-        <div>
-            <img src={data.img} />
-            <h3>{data.name}</h3>
-            <p>{data.profession}</p>
+        <div className={styles.studentCard}>
+            <div>
+              <img src={studentPic} width={100} height={100} alt="student image" />
+             {/*  <a href={portfolioUrl}>
+                <p className={styles.portfolio}>Portfolio</p>
+              </a>
+    */}
+            </div>
+            <div>
+              <h3>{name}</h3>
+              <p className={styles.profession}>{profession}</p>
+              <p>{email}</p>
+            </div>
         </div>
     );
 }
--- src/pages/students.jsx
+++ src/pages/students.jsx
@@ -1,15 +1,23 @@
 import studentData from '../../public/studentData.js';
 import StudentCard from '../components/StudentCard.jsx';
+import styles from '../styles/students.module.css';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default function Students() {
     return (
-        <div>
-            <div>TITLE</div>
-            <div>
-                {studentData.map((data) => {
-                    <StudentCard key={data.id} {...data} />;
-                })}
-            </div>
+      <div>
+        <div className={styles.arrowLeft}>
+          <Link to="/">
+            <FaArrowLeft />
+          </Link>
         </div>
+        <h1 className={styles.title}>STUDENTS</h1>
+        <div className={styles.cardContainer}>
+          {studentData.map((data) => {
+            return <StudentCard key={data.id} {...data} />;
+          })}
+        </div>
+      </div>
     );
 }
