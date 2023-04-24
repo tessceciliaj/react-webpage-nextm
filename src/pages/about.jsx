855820d20c3c772e984a3e49b1fc1eba2a457379
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-04-24 16:14:46 +0200

    create about all this page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styles from '../styles/about.module.css'
+import student from '../assets/student1.png'
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default function about() {
   return (
-    <div>about</div>
+    <div>
+       <div>
+          <Link to="/">
+            <FaArrowLeft className={styles.arrowLeft} />
+          </Link>
+        </div>
+      <div className={styles.upperContainer}>
+        <h1>Im a big fan of Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div className={styles.lowerContainer}>
+        <div className={styles.innerContainer}>
+          <img src={student} width={50} height={50} alt="student picture"/>
+          <p>John Doe</p>
+        </div>
+        <p className={styles.quote}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
   )
 }
