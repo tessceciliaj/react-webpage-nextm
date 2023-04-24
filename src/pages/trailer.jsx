76f0eb474f5526b3225ed4ba382c4cbb8dca5fce
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-04-24 16:51:43 +0200

    add trailer page + basic styles

--- src/pages/trailer.jsx
+++ src/pages/trailer.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import styles from '../styles/trailer.module.css'
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-export default function tiktokfilter() {
+export default function Trailer() {
   return (
-    <div>tiktokfilter</div>
+    <div>
+       <div className={styles.upperContainer}>
+          <Link to="/">
+            <FaArrowLeft className={styles.arrowLeft}/>
+          </Link>
+          <h1>Trailer</h1>
+        </div>
+        <div className={styles.videoContainer}>
+        /*VIDEO*/
+        </div>
+        <div className={styles.lowerContainer}>
+          <h2>Watch our trailer.</h2>
+          <h3>Introduction to our future proof talent!</h3>
+        </div>
+    </div>
   )
 }
