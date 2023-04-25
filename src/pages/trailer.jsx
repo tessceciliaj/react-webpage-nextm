d50106c87f53bcd0f2327f364f9296367b88ca71
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-04-25 14:57:05 +0200

    add youtube-react and implement youtube video

--- src/pages/trailer.jsx
+++ src/pages/trailer.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import styles from '../styles/trailer.module.css'
 import { FaArrowLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import YouTube from 'react-youtube';
+
 
 export default function Trailer() {
+
+  const opts = {
+    height: '340',
+    width: '340',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div>
        <div className={styles.upperContainer}>
@@ -12,7 +23,7 @@ export default function Trailer() {
           </Link>
         </div>
         <div className={styles.videoContainer}>
-        /*VIDEO*/
+        <YouTube videoId="QLZx_VrPbB8" opts={opts}/>
         </div>
         <div className={styles.lowerContainer}>
           <h2>Watch our trailer.</h2>
