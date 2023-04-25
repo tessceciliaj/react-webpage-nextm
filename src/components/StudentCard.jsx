3f1251af8e3862c5c0981914dd549cb143643d9c
Ida Mäkelä <idamakela98@gmail.com>
2023-04-25 21:03:27 +0200

    logic for conditionally rendered img

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -4,77 +4,79 @@ import { FaEnvelope, FaLinkedin } from 'react-icons/fa';
 import { useState } from 'react';
 
 export default function StudentCard({
-    img,
-    name,
-    profession,
-    portfolioUrl,
-    portfolio,
-    desc,
-    email,
-    linkedinUrl,
-    linkedin,
+  img,
+  name,
+  profession,
+  portfolioUrl,
+  portfolio,
+  desc,
+  email,
+  linkedinUrl,
+  linkedin,
 }) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleClick = () => {
-        setOpen(!open);
-    };
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
-    return (
-        <div className={styles.cardContainer}>
-            <div className={styles.studentCard} onClick={handleClick}>
-                <div className={styles.upper}>
-                    <img src={studentPic} width={200} height={200} alt='student image' />
-                </div>
-                <div className={styles.lower}>
-                    <h3>{name}</h3>
-                    <p className={styles.profession}>{profession}</p>
-                    <div className={styles.links}>
-                        {portfolio ? (
-                            <a href={portfolioUrl}>
-                                <p className={styles.link}>Portfolio</p>
-                            </a>
-                        ) : (
-                            <a href={portfolioUrl}>
-                                <p className={styles.link} id={styles.disabled}>
-                                    Portfolio
-                                </p>
-                            </a>
-                        )}
-                        <span>|</span>
-                        {open ? (
-                            <div className={styles.link}>close</div>
-                        ) : (
-                            <div className={styles.link}>Read more</div>
-                        )}
-                    </div>
-                </div>
-            </div>
-            {open && (
-                <div className={styles.bigCard}>
-                    <div className={styles.moreInfo}>
-                        <div>
-                            <p>{`"${desc}"`}</p>
-                        </div>
-                        <div className={styles.contactInfo}>
-                            <div className={styles.linkedinbox}>
-                                <a href={linkedinUrl}>
-                                    <div className={styles.icon}>
-                                        <FaLinkedin />
-                                    </div>
-                                    <p>LinkedIn</p>
-                                </a>
-                            </div>
-                            <div className={styles.emailbox}>
-                                <div className={styles.icon}>
-                                    <FaEnvelope />
-                                </div>
-                                <p className={styles.email}>{email}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
+  const imgUrl = img.length !== 0 ? img : studentPic;
+
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.studentCard} onClick={handleClick}>
+        <div className={styles.upper}>
+          <img src={imgUrl} width={200} height={200} alt='student image' />
         </div>
-    );
+        <div className={styles.lower}>
+          <h3>{name}</h3>
+          <p className={styles.profession}>{profession}</p>
+          <div className={styles.links}>
+            {portfolio ? (
+              <a href={portfolioUrl}>
+                <p className={styles.link}>Portfolio</p>
+              </a>
+            ) : (
+              <a href={portfolioUrl}>
+                <p className={styles.link} id={styles.disabled}>
+                  Portfolio
+                </p>
+              </a>
+            )}
+            <span>|</span>
+            {open ? (
+              <div className={styles.link}>close</div>
+            ) : (
+              <div className={styles.link}>Read more</div>
+            )}
+          </div>
+        </div>
+      </div>
+      {open && (
+        <div className={styles.bigCard}>
+          <div className={styles.moreInfo}>
+            <div>
+              <p>{`"${desc}"`}</p>
+            </div>
+            <div className={styles.contactInfo}>
+              <div className={styles.linkedinbox}>
+                <a href={linkedinUrl}>
+                  <div className={styles.icon}>
+                    <FaLinkedin />
+                  </div>
+                  <p>LinkedIn</p>
+                </a>
+              </div>
+              <div className={styles.emailbox}>
+                <div className={styles.icon}>
+                  <FaEnvelope />
+                </div>
+                <p className={styles.email}>{email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
