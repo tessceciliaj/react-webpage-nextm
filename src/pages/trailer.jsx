2b7f87d23dbda6b5a85b35291fbc1f102d0f121d
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-04-25 20:37:47 +0200

    change youtube video ID

--- src/pages/trailer.jsx
+++ src/pages/trailer.jsx
@@ -17,18 +17,18 @@ export default function Trailer() {
 
   return (
     <div>
-       <div className={styles.upperContainer}>
-          <Link to="/">
-            <FaArrowLeft className={styles.arrowLeft}/>
-          </Link>
-        </div>
-        <div className={styles.videoContainer}>
-        <YouTube videoId="QLZx_VrPbB8" opts={opts}/>
-        </div>
-        <div className={styles.lowerContainer}>
-          <h2>Watch our trailer.</h2>
-          <h3>Introduction to our future proof talent!</h3>
-        </div>
+      <div className={styles.upperContainer}>
+        <Link to="/">
+          <FaArrowLeft className={styles.arrowLeft} />
+        </Link>
+      </div>
+      <div className={styles.videoContainer}>
+        <YouTube videoId="MQFIhbYUndc" opts={opts} />
+      </div>
+      <div className={styles.lowerContainer}>
+        <h2>Watch our trailer.</h2>
+        <h3>Introduction to our future proof talent!</h3>
+      </div>
     </div>
-  )
+  );
 }
